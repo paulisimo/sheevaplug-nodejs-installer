f669c5c6ce03abb998c28aaecb62601e1118b1eb
Rico Chen <ricochen27@gmail.com>
2013-05-26 23:11:49 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# About
+node.js installation one-liner for Debian (7.0) on sheevaplug
+
+# Examine the script
+	curl -L https://raw.github.com/midnightcodr/sheevaplug-nodejs-installer/master/setup.sh
+
+# Install
+	curl -L https://raw.github.com/midnightcodr/sheevaplug-nodejs-installer/master/setup.sh | bash -s
+
+# Credits
+	http://www.yoovant.com/how-install-node-js-0-8-arm-based-sbc-fox-board-g20/
---
+++ setup.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# install packages
+sudo aptitude install build-essential openssl libssl-dev
+# download nodejs source code
+orig_dir=$(pwd)
+work_dir=/tmp
+cd $work_dir
+rm -rf $work_dir/node-v0.10.8
+wget -O - http://nodejs.org/dist/v0.10.8/node-v0.10.8.tar.gz|tar xz
+# patch files
+patch_dir=$work_dir/node-v0.10.8/deps/v8/
+p1=$patch_dir/SConstruct.patch
+p2=$patch_dir/src_arm_macro.patch
+echo "--- SConstruct	2013-05-24 18:06:33.000000000 -0400
++++ ../../../node-v0.10.8-custom/deps/v8/SConstruct	2013-05-26 19:56:08.680350873 -0400
+@@ -80,8 +80,8 @@
+   },
+   'gcc': {
+     'all': {
+-      'CCFLAGS':      ['\$DIALECTFLAGS', '\$WARNINGFLAGS'],
+-      'CXXFLAGS':     ['-fno-rtti', '-fno-exceptions'],
++      'CCFLAGS':      ['\$DIALECTFLAGS', '\$WARNINGFLAGS', '-march=armv5t', '-mthumb-interwork'],
++      'CXXFLAGS':     ['-fno-rtti', '-fno-exceptions', '-march=armv5t', '-mthumb-interwork'],
+     },
+     'visibility:hidden': {
+       # Use visibility=default to disable this.
+@@ -159,11 +159,11 @@
+       },
+       'armeabi:softfp' : {
+         'CPPDEFINES' : ['USE_EABI_HARDFLOAT=0'],
+-        'vfp3:on': {
+-          'CPPDEFINES' : ['CAN_USE_VFP_INSTRUCTIONS']
+-        },
++#        'vfp3:on': {
++#          'CPPDEFINES' : ['CAN_USE_VFP_INSTRUCTIONS']
++#        },
+         'simulator:none': {
+-          'CCFLAGS':     ['-mfloat-abi=softfp'],
++          'CCFLAGS':     ['-mfloat-abi=soft'],
+         }
+       },
+       'armeabi:hard' : {
+" > $p1
+
+echo "--- src/arm/macro-assembler-arm.cc	2013-05-24 18:06:33.000000000 -0400
++++ ../../../node-v0.10.8-custom/deps/v8/src/arm/macro-assembler-arm.cc	2013-05-26 19:56:50.880349996 -0400
+@@ -61,9 +61,9 @@
+ // We do not support thumb inter-working with an arm architecture not supporting
+ // the blx instruction (below v5t).  If you know what CPU you are compiling for
+ // you can use -march=armv7 or similar.
+-#if defined(USE_THUMB_INTERWORK) && !defined(CAN_USE_THUMB_INSTRUCTIONS)
+-# error \"For thumb inter-working we require an architecture which supports blx\"
+-#endif
++//#if defined(USE_THUMB_INTERWORK) && !defined(CAN_USE_THUMB_INSTRUCTIONS)
++//# error \"For thumb inter-working we require an architecture which supports blx\"
++//#endif
+ 
+ 
+ // Using bx does not yield better code, so use it only when required
+" > $p2
+cd $patch_dir
+patch -p0 < $p1
+patch -p0 < $p2
+
+# configure, make and install
+cd ../..
+./configure && make && sudo make install
+cd $orig_dir
